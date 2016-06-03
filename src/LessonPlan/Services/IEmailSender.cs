632090fb3ba70c3e3e023fee083d06215b727497
Chad Chandler <chad.chandler@outlook.com>
2016-06-03 15:45:10 -0400

    Add project files.

---
+++ src/LessonPlan/Services/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LessonPlan.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
---
+++ src/LessonPlan/Services/ISmsSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LessonPlan.Services
+{
+    public interface ISmsSender
+    {
+        Task SendSmsAsync(string number, string message);
+    }
+}
